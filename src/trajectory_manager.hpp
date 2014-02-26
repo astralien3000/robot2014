60dbffa12eff8d3bbaae674a8bf9a78f00575a33
ldauphin <astralien3000@yahoo.fr>
2014-02-26 15:01:18 +0100

    Added methods to trajectory_manager

--- src/trajectory_manager.hpp
+++ src/trajectory_manager.hpp
@@ -14,10 +14,31 @@ private:
   Output< Vect<2, s32> >& _robot;
   Input< Vect<2, s32> >& _pos;
 
+  Vect<2, s32> _src;
+  Vect<2, s32> _dst;
+  s32 _curve;
+
 public:
+  //! \brief Choose if the robot must go forward or can go backward if it is faster
+  enum TrajectoryMode {
+    MODE_ANGLE_2PI,
+    MODE_ANGLE_PI
+  };
+
+public:
+  //! \brief Constructor
+  //! \param robot : A robot controller with dist/angle asserv
+  //! \param pos : A device to get the x,y,angle position of the robot
   TrajectoryManager(Output< Vect<2, s32> >& robot, Input< Vect<2, s32> >& pos);
 
-  
+  //! \brief Set the new point to reach from current position
+  void gotoPosition(Vect<2, s32> pos, s32 curve = 0);
+
+  //! \brief Stop the trajectory in order to restart it later.
+  void pause(void);
+
+  //! \brief Resume the paused trajectory
+  void resume(void);
 };
 
 #endif//TRAJECTORY_MANAGER_HPP
