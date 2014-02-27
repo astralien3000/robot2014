de2f79297ef7e4193c1fa006e86f922e49f6a84f
Nicolas <nicolas@debianTestingVM>
2014-02-27 11:33:59 +0100

    secure robot done. needs to be compiled/tested .. as always :)

---
+++ src/secure_robot.cpp
@@ -0,0 +1,38 @@
+#include "secure_robot.hpp"
+
+
+SecureRobot::SecureRobot(Output< Vect<2, s32> >& robot, Input<bool>& skd_l, Input<bool>& skd_r) {
+  this->_robot = robot;
+  this->_skd_l = skd_l;
+  this->_skd_r = skd_r;
+  this->_state = false;
+  Task check([this](void) {
+      this->_state = this->_skd_l.getValue() || this->_skd_r.getValue();
+      if (this->_state) {
+	Vect<2, s32> zero(0, 0);
+	this->setValue(zero);
+      }
+    });
+  check.setRepeat();
+  check.setPeriod(100000); //check 10 times per second if robot is skating
+  Scheduler::instance().addTask(check);
+}
+
+bool SecureRobot::getValue(void) { //what is the best solution ?
+  return this->_state;
+  //return (this->_skd_l.getValue() && this->_skd_r.getValue());
+}
+
+void SecureRobot::setValue(Vect<2, s32> command) {
+  if (this->_state) {
+    //robot is skating, send "DO NOT MOVE"
+    Vect<2, s32> zero(0, 0);
+    this->robot.setValue(zero);
+  } else {
+    this->_robot.setValue(command);
+  }
+}
+
+void SecureRobot::unlock(void) {
+  this->_state = false;
+}
--- src/secure_robot.hpp
+++ src/secure_robot.hpp
@@ -1,6 +1,9 @@
 #ifndef SECURE_ROBOT_HPP
 #define SECURE_ROBOT_HPP
 
+#include <system/task.hpp>
+#include <system/scheduler.hpp>
+
 #include <base/integer.hpp>
 
 #include <device/intput.hpp>
@@ -16,11 +19,13 @@
   the robot controller, but if the robot is skating, it prevents to
   continue.
 
+  (_state == true) <=> robot is skating
  */
 class SecureRobot : public Output<Vect<2, s32>>, public Input<bool> {
 private:
   Output< Vect<2, s32> >& _robot;
   Input<bool>& _skd_l, _skd_r;
+  bool _state;
 
 public:
   SecureRobot(Output< Vect<2, s32> >& robot, Input<bool>& skd_l, Input<bool>& skd_r);
