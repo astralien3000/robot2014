3ae09f165d6ff2e1781e822c62369eb1fc72ec1d
astralien3000 <astralien3000@yahoo.fr>
2014-05-29 10:39:59 +0200

    CaptureAction header

---
+++ src/capture_action.hpp
@@ -0,0 +1,21 @@
+#ifndef CAPTURE_ACTION_HPP
+#define CAPTURE_ACTION_HPP
+
+#include "action.hpp"
+
+class CaptureAction : public Action {
+public:
+  CaptureAction(const Vect<2, s32>& pos, const Vect<2, s32>& mamouth, s32 number);
+  
+  s16 priority(void);
+  Vect<2, s32> controlPoint(void);
+  enum Error doAction(void);
+
+private:
+  Vect<2, s32> _pos;
+  Vect<2, s32> _mamouth;
+
+  
+};
+
+#endif//CAPTURE_ACTION_HPP
